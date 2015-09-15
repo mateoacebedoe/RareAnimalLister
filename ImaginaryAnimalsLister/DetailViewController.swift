bfe928ef901d321098bb128bd90de96614959b9c
Mateo Acebedo <ma698@cornell.edu>
2015-09-15 10:35:13 -0700

    json file

--- ImaginaryAnimalsLister/DetailViewController.swift
+++ ImaginaryAnimalsLister/DetailViewController.swift
@@ -19,9 +19,19 @@ class DetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let url = animal?.imageURL,
-            let imageData = NSData(contentsOfURL: url) {
-                self.imageView.image = UIImage(data: imageData)
+        if let animal = animal {
+            self.nameLabel.text = animal.name
+            self.heightLabel.text = "(animal.height)"
+            self.locationLabel.text = animal.location
+            self.dateLastSeenLabel.text = animal.dateLastSeen
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if let url = animal?.imageURL, let imageData = NSData(contentsOfURL: url) {
+            self.imageView.image = UIImage(data: imageData)
         }
     }
 }
